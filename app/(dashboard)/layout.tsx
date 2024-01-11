67cd15a8fd7e4767ab91e1dd631ea9e7c7090003
Ashish Kumawat <ashishskyblue@gmail.com>
2024-01-11 19:28:54 +0530

    :iphone: added mobile sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,14 +1,18 @@
 import { TLayoutProps } from "@/lib/typings";
 import React from "react";
 import Sidebar from "./_components/sidebar";
+import Navbar from "./_components/navbar";
 
 const DashboardLayout = ({ children }: TLayoutProps) => {
   return (
     <div className="h-full">
-      <div className="hidden md:flex h-full bg-gray-50 w-56 flex-col z-50">
+      <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
+        <Navbar />
+      </div>
+      <div className="hidden md:flex flex-col h-full bg-gray-50 w-56 fixed inset-y-0 z-50">
         <Sidebar />
       </div>
-      {children}
+      <main className="md:pl-56 min-h-full">{children}</main>
     </div>
   );
 };
