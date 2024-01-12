cc3ea235a92825a52647e10cea7452c431a2aa95
Ashish Kumawat <ashishskyblue@gmail.com>
2024-01-12 10:48:00 +0530

    added navbar for dashboard

--- app/(dashboard)/_components/navbar/index.tsx
+++ app/(dashboard)/_components/navbar/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MobileSidebar from "../mobile-sidebar";
+import NavbarRoutes from "@/components/custom/navbar-routes";
 
 const Navbar = () => {
   return (
     <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
       <MobileSidebar />
+      <NavbarRoutes />
     </div>
   );
 };
--- app/(dashboard)/_components/sidebar/logo.tsx
+++ app/(dashboard)/_components/sidebar/logo.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
 
 const Logo = () => {
-  return <Image height={60} width={60} alt="logo" src={"/logo.svg"} />;
+  return (
+    <div className="flex gap-x-2">
+      <Image height={60} width={60} alt="logo" src={"/logo.svg"} />
+      <h1 className="text-3xl text-sky-800 font-bold font-mono">Lorem.</h1>
+    </div>
+  );
 };
 
 export default Logo;
