fda02a7edd47f5992b90592d445504b825a779ce
Ashish Kumawat <ashishskyblue@gmail.com>
2024-01-13 16:25:49 +0530

    :fire: created custom typed cva component

---
+++ components/custom/icon-badge.tsx
@@ -0,0 +1,54 @@
+import { LucideIcon } from "lucide-react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const backgroundVariants = cva(
+  "flex items-center justify-center rounded-full",
+  {
+    variants: {
+      variant: {
+        default: "bg-sky-100",
+        success: "bg-emrald-100",
+      },
+      size: {
+        default: "p-2",
+        sm: "p-1",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+const iconVariants = cva("", {
+  variants: {
+    variant: {
+      default: "text-sky-100",
+      success: "text-emrald-700",
+    },
+    size: {
+      default: "h-8 w-8",
+      sm: "h-4 w-4",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
+
+type BackgroundVariantsProps = VariantProps<typeof backgroundVariants>;
+type IconVariantsProps = VariantProps<typeof iconVariants>;
+
+interface IconBadgeProps extends BackgroundVariantsProps, IconVariantsProps {
+  icon: LucideIcon;
+}
+
+export const IconBadge = ({ icon: Icon, variant, size }: IconBadgeProps) => {
+  <div className={cn(backgroundVariants({ variant, size }))}>
+    <Icon className={cn(iconVariants({ variant, size }))} />
+  </div>;
+};
