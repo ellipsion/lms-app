408967eb02a4646fa1a90aa76f83c0df185e83a2
Ashish Kumawat <ashishskyblue@gmail.com>
2024-08-08 12:09:21 +0530

    fix: build errors

--- app/(course)/courses/[courseId]/_components/course-sidebar.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-import SidebarRoutes from "./sidebar-routes";
 import { CourseWithChapters } from "../layout";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
